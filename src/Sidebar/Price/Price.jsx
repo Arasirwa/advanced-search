883ec8888418eb4a0436c3b3ae2bcee113652972
William Arasirwa <mageboarasirwa03@gmail.com>
2024-05-30 09:18:42 +0300

    first-input-refactor

--- src/Sidebar/Price/Price.jsx
+++ src/Sidebar/Price/Price.jsx
@@ -1,29 +1,11 @@
+import Input from '../../Components/Input'
 import './Price.css'
 
 const Price = () => {
   return (
     <div className='ml'>
       <h2 className="sidebar-title price-title">Price</h2>
-      <label className='sidebar-label-container'>
-        <input type="radio" name='test2' />
-        <span className='checkmark'></span>All
-      </label>
-      <label className='sidebar-label-container'>
-        <input type="radio" name='test2' />
-        <span className='checkmark'></span>$0 - $50
-      </label>
-      <label className='sidebar-label-container'>
-        <input type="radio" name='test2' />
-        <span className='checkmark'></span>$50 - $100
-      </label>
-      <label className='sidebar-label-container'>
-        <input type="radio" name='test2' />
-        <span className='checkmark'></span>$100 - $150
-      </label>
-      <label className='sidebar-label-container'>
-        <input type="radio" name='test2' />
-        <span className='checkmark'></span>$150 - $200
-      </label>
+      <Input />
     </div>
   )
 }
